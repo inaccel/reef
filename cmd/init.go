0372c7b7aad937c33d73d3764447c3540249dc52
Elias Koromilas <elias.koromilas@gmail.com>
2022-09-06 16:12:02 +0300

    Simplify self-signed certificate generation

--- cmd/init.go
+++ cmd/init.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
+	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	"github.com/cert-manager/cert-manager/pkg/util/pki"
 	"github.com/urfave/cli/v2"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -23,45 +18,11 @@ var initCommand = &cli.Command{
 	Usage:     "Update SSL certification/key files",
 	ArgsUsage: " ",
 	Action: func(context *cli.Context) error {
-		now := time.Now()
-
 		kube, err := config.GetConfig()
 		if err != nil {
 			return err
 		}
 		api, err := client.New(kube, client.Options{})
-		if err != nil {
-			return err
-		}
-
-		caSerialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 8*20))
-		if err != nil {
-			return err
-		}
-		ca := &x509.Certificate{
-			SerialNumber: caSerialNumber,
-			Subject: pkix.Name{
-				Organization: []string{
-					"inaccel.com",
-				},
-			},
-			NotBefore: now,
-			NotAfter:  now.AddDate(1, 0, 0),
-			KeyUsage:  x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-			ExtKeyUsage: []x509.ExtKeyUsage{
-				x509.ExtKeyUsageClientAuth,
-				x509.ExtKeyUsageServerAuth,
-			},
-			BasicConstraintsValid: true,
-			IsCA:                  true,
-		}
-
-		caPriv, err := rsa.GenerateKey(rand.Reader, 4096)
-		if err != nil {
-			return err
-		}
-
-		caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, caPriv.Public(), caPriv)
 		if err != nil {
 			return err
 		}
@@ -73,12 +34,86 @@ var initCommand = &cli.Command{
 			return err
 		}
 
+		var cert string
+		if _, err := os.Stat(context.String("cert")); err == nil {
+			cert = context.String("cert")
+		}
+		var key string
+		if _, err := os.Stat(context.String("key")); err == nil {
+			key = context.String("key")
+		}
+
+		var caBundle []byte
+		if len(cert) == 0 || len(key) == 0 {
+			certificate := &certmanagerv1.Certificate{
+				Spec: certmanagerv1.CertificateSpec{
+					Subject: &certmanagerv1.X509Subject{
+						Organizations: []string{
+							"inaccel.com",
+						},
+					},
+					DNSNames: []string{
+						mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
+						strings.Join([]string{
+							mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
+							mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Namespace,
+						}, "."),
+						strings.Join([]string{
+							mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
+							mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Namespace,
+							"svc",
+						}, "."),
+					},
+				},
+			}
+
+			ca, err := pki.GenerateTemplate(certificate)
+			if err != nil {
+				return err
+			}
+
+			caPrivateKey, err := pki.GeneratePrivateKeyForCertificate(certificate)
+			if err != nil {
+				return err
+			}
+
+			caPrivateKeyBytes, err := pki.EncodePrivateKey(caPrivateKey, certmanagerv1.PKCS8)
+			if err != nil {
+				return err
+			}
+
+			caPublicKey, err := pki.PublicKeyForPrivateKey(caPrivateKey)
+			if err != nil {
+				return err
+			}
+
+			caBytes, _, err := pki.SignCertificate(ca, ca, caPublicKey, caPrivateKey)
+			if err != nil {
+				return err
+			}
+
+			if err := os.MkdirAll(filepath.Dir(context.String("cert")), os.ModePerm); err != nil {
+				return err
+			}
+			if err := os.WriteFile(context.String("cert"), caBytes, os.ModePerm); err != nil {
+				return err
+			}
+
+			if err := os.MkdirAll(filepath.Dir(context.String("key")), os.ModePerm); err != nil {
+				return err
+			}
+			if err := os.WriteFile(context.String("key"), caPrivateKeyBytes, os.ModePerm); err != nil {
+				return err
+			}
+
+			caBundle = caBytes
+		}
+
 		for i := range mutatingWebhookConfiguration.Webhooks {
 			if mutatingWebhookConfiguration.Webhooks[i].Name == "reef.inaccel.com" {
-				mutatingWebhookConfiguration.Webhooks[i].ClientConfig.CABundle = pem.EncodeToMemory(&pem.Block{
-					Type:  "CERTIFICATE",
-					Bytes: caBytes,
-				})
+				if caBundle != nil {
+					mutatingWebhookConfiguration.Webhooks[i].ClientConfig.CABundle = caBundle
+				}
 
 				mutatingWebhookConfiguration.Webhooks[i].Rules = []admissionregistrationv1.RuleWithOperations{
 					{
@@ -94,69 +129,6 @@ var initCommand = &cli.Command{
 		}
 
 		if err := api.Update(context.Context, mutatingWebhookConfiguration); err != nil {
-			return err
-		}
-
-		serverSerialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 8*20))
-		if err != nil {
-			return err
-		}
-		server := &x509.Certificate{
-			SerialNumber: serverSerialNumber,
-			Subject: pkix.Name{
-				Organization: []string{
-					"inaccel.com",
-				},
-			},
-			NotBefore: now,
-			NotAfter:  now.AddDate(1, 0, 0),
-			KeyUsage:  x509.KeyUsageDigitalSignature,
-			ExtKeyUsage: []x509.ExtKeyUsage{
-				x509.ExtKeyUsageServerAuth,
-			},
-			BasicConstraintsValid: true,
-			IsCA:                  false,
-			DNSNames: []string{
-				mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
-				strings.Join([]string{
-					mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
-					mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Namespace,
-				}, "."),
-				strings.Join([]string{
-					mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Name,
-					mutatingWebhookConfiguration.Webhooks[0].ClientConfig.Service.Namespace,
-					"svc",
-				}, "."),
-			},
-		}
-
-		serverPriv, err := rsa.GenerateKey(rand.Reader, 4096)
-		if err != nil {
-			return err
-		}
-
-		serverBytes, err := x509.CreateCertificate(rand.Reader, server, ca, serverPriv.Public(), caPriv)
-		if err != nil {
-			return err
-		}
-
-		if err := os.MkdirAll(filepath.Dir(context.String("key")), os.ModePerm); err != nil {
-			return err
-		}
-		if err := os.WriteFile(context.String("key"), pem.EncodeToMemory(&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(serverPriv),
-		}), os.ModePerm); err != nil {
-			return err
-		}
-
-		if err := os.MkdirAll(filepath.Dir(context.String("cert")), os.ModePerm); err != nil {
-			return err
-		}
-		if err := os.WriteFile(context.String("cert"), pem.EncodeToMemory(&pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: serverBytes,
-		}), os.ModePerm); err != nil {
 			return err
 		}
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/bombsimon/logrusr/v3 v3.0.0
+	github.com/cert-manager/cert-manager v1.9.1
 	github.com/sirupsen/logrus v1.9.0
 	github.com/urfave/cli/v2 v2.14.1
 	github.com/wI2L/jsondiff v0.2.0
@@ -13,6 +14,7 @@ require (
 )
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20200615164410-66371956d46c // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -22,15 +24,17 @@ require (
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.1 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -50,6 +54,7 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
@@ -62,10 +67,14 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.25.0 // indirect
 	k8s.io/client-go v0.25.0 // indirect
+	k8s.io/component-base v0.25.0 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
+	k8s.io/kube-aggregator v0.24.2 // indirect
 	k8s.io/kube-openapi v0.0.0-20220803162953-67bda5d908f1 // indirect
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
+	sigs.k8s.io/gateway-api v0.4.3 // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
